4705fe7c7124ac5852aa2d7fc851980faf6f1e6d
kuffa2207 <kuffa2207@gmail.com>
2020-05-12 23:16:52 +0300

    Update add.h

--- include/add.h
+++ include/add.h
@@ -1,6 +1,43 @@
 #ifndef INCLUDE_ADD_H_
 #define INCLUDE_ADD_H_
 
-int add(int x, int y);
+#pragma once
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <locale.h>
+#include <iostream>
 
+using namespace std;
+
+class Bankomat
+{
+public:
+
+private:
+
+};
+
+class ProcCenter
+{
+public:
+	void getp();
+private:
+	vector <Help> person;
+};
+
+class Help {
+public:
+	Help(string _firstname, string _secondname, string _thname, int _ID, int _money, int _PIN, bool _block);
+	void seth(string _firstname, string _secondname, string _thname, int _ID, int _money, int _PIN, bool _block);
+	~Help() {};
+private:
+	string firstname;
+	string secondname;
+	string thname;
+	int ID;
+	int money;
+	int PIN;
+	bool block;
+};
 #endif  // INCLUDE_ADD_H_
